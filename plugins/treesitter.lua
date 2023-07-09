0103aed551b1f06c4e93b30f3c884a7381df45fd
bigzhu <vermiliondun@gmail.com>
2023-07-09 11:47:09 +0800

    can't lock treesitter version from lazy

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,7 +1,5 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  -- 锁定版本, 临时解决慢的问题: https://github.com/nvim-treesitter/nvim-treesitter/issues/4945
-  version = "33eb472b459f1d2bf49e16154726743ab3ca1c6d",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
