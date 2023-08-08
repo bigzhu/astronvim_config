2036bb34e5ce2ef7b65c693d8010a1b02fba248f
bigzhu <vermiliondun@gmail.com>
2023-08-08 10:08:48 +0800

    添加结构插件

--- plugins/user.lua
+++ plugins/user.lua
@@ -81,4 +81,15 @@ return {
       end, { noremap = false, expr = true })
     end,
   },
+  {
+    "stevearc/aerial.nvim",
+    config = function()
+      local aerial = require "aerial"
+      aerial.setup {
+        attach_mode = "window",
+        open_automatic = true,
+        backends = { "treesitter", "lsp", "markdown" },
+      }
+    end,
+  },
 }
