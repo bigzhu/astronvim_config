8d4afef843655bf2aaec4e9e5112732e4c085b1d
bigzhu <vermiliondun@gmail.com>
2023-08-21 00:05:44 +0800

    锁定 treesitter 的版本

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,9 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  commit = "33eb472b459f1d2bf49e16154726743ab3ca1c6d",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "graphql",
+      "lua",
       "markdown",
       "markdown_inline",
     })
