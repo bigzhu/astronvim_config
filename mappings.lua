04c29d0e87e98d5fa708bca3237c69c197e76cdb
bigzhu <vermiliondun@gmail.com>
2023-09-01 10:41:25 +0800

    change to daily

--- mappings.lua
+++ mappings.lua
@@ -8,21 +8,22 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
-      end,
-      desc = "Pick to close",
-    },
+    -- ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    -- ["<leader>bD"] = {
+    --   function()
+    --     require("astronvim.utils.status").heirline.buffer_picker(
+    --       function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+    --     )
+    --   end,
+    --   desc = "Pick to close",
+    -- },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
+    -- ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    ["<C-g>"] = { ":SearchMD ", desc = "Cheese search and generate" },
+    -- ["<C-g>"] = { ":SearchMD ", desc = "Cheese search and generate" },
+    ["<C-g>"] = { "<cmd>ObsidianToday<CR>", desc = "Cheese create a new daily note" },
     ["<C-f>"] = { "<cmd>ObsidianQuickSwitch<CR>", desc = "Cheese search by file name" },
     ["<C-j>"] = { "<cmd>ObsidianSearch<CR>", desc = "Cheese search by content" },
     ["<F4>"] = { "<cmd>ObsidianOpen<CR>", desc = "Open current note in the Obsidian app" },
