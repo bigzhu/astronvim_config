bbfbeea4f694d5af7dad2bed08267a7e4515746e
bigzhu <vermiliondun@gmail.com>
2023-09-07 15:09:02 +0800

    加入 riverpod snip

--- plugins/user.lua
+++ plugins/user.lua
@@ -46,4 +46,18 @@ return {
       }
     end,
   },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      -- include the default astronvim config that calls the setup call
+      require "plugins.configs.luasnip"(plugin, opts)
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        -- this can be used if your configuration lives in ~/.config/nvim
+        -- if your configuration lives in ~/.config/astronvim, the full path
+        -- must be specified in the next line
+        paths = { "./lua/user/snippets/flutter-riverpod-snippets" },
+      }
+    end,
+  },
 }
